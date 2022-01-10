ddfbea49706ff8fdd82d7205bca6fc8c573bbbf8
Suin Kim <soo.officer@gmail.com>
2022-01-10 14:54:40 +0900

    PageBlog 컴포넌트 생성 및 스타일 적용

---
+++ src/pages/PageBlog.jsx
@@ -0,0 +1,9 @@
+function PageBlog() {
+  return (
+    <div className="text-gray-500 hover:text-red-500 hover:border-red-500 border-b-4">
+      PageBlog
+    </div>
+  );
+}
+
+export default PageBlog;
