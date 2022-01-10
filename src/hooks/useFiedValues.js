05de6e804ff13881603aba4017a9eb44d8fb3899
Suin Kim <soo.officer@gmail.com>
2022-01-10 13:35:42 +0900

    review update 구현

--- src/hooks/useFiedValues.js
+++ src/hooks/useFiedValues.js
@@ -1,9 +1,10 @@
-import { useState } from 'react';
+import { useCallback, useState } from 'react';
 
 function useFieldValues(initialValues) {
   const [fieldValues, setFieldValues] = useState(initialValues);
 
-  const handleFieldChange = (e) => {
+  // 함수 객체를 생성할 때 의존성이 걸린 값이 변경시에만 함수를 재생성
+  const handleFieldChange = useCallback((e) => {
     const { name, value } = e.target;
     setFieldValues((prevFieldValues) => {
       return {
@@ -11,10 +12,11 @@ function useFieldValues(initialValues) {
         [name]: value,
       };
     });
-  };
-  const clearFieldValues = () => {
+  }, []);
+
+  const clearFieldValues = useCallback(() => {
     setFieldValues(initialValues);
-  };
+  }, []);
 
   return {
     fieldValues,
--- src/pages/reviews/ReviewForm.jsx
+++ src/pages/reviews/ReviewForm.jsx
@@ -3,25 +3,60 @@ import ReviewForm from 'components/ReviewForm';
 import useFieldValues from 'hooks/useFiedValues';
 import { useNavigate, useParams } from 'react-router-dom';
 import Axios from 'axios';
+import { useEffect, useState } from 'react/cjs/react.development';
 
 function PageReviewForm() {
   // 상탯값 정의. 훅 호출
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+
   const navigate = useNavigate();
   const { reviewId } = useParams();
-  const { fieldValues, handleFieldChange } = useFieldValues({
-    score: 5,
-    content: '',
-  });
+  const { fieldValues, handleFieldChange, setFieldValues, clearFieldValues } =
+    useFieldValues({
+      score: 5,
+      content: '',
+    });
+
+  useEffect(() => {
+    const fetchReview = async () => {
+      setLoading(true);
+      setError(null);
+
+      const url = `http://localhost:8000/shop/api/reviews/${reviewId}/`;
+      try {
+        const response = await Axios.get(url);
+        setFieldValues(response.data);
+      } catch (e) {
+        setError(e);
+      }
+      setLoading(false);
+    };
+    if (reviewId) fetchReview();
+    else clearFieldValues();
+  }, [reviewId]);
 
   // 다양한 함수를 정의
   const saveReview = async () => {
-    const url = 'http://localhost:8000/shop/api/reviews/';
+    setLoading(true);
+    setError(null);
+
+    const url = !reviewId
+      ? 'http://localhost:8000/shop/api/reviews/'
+      : `http://localhost:8000/shop/api/reviews/${reviewId}/`;
+
     try {
-      await Axios.post(url, fieldValues);
+      if (!reviewId) {
+        await Axios.post(url, fieldValues);
+      } else {
+        await Axios.post(url, fieldValues);
+      }
       navigate('/reviews/');
     } catch (e) {
+      setError(e);
       console.error(e);
     }
+    setLoading(false);
   };
 
   // 표현 by jsx
@@ -35,6 +70,7 @@ function PageReviewForm() {
         fieldValues={fieldValues}
         handleFieldChange={handleFieldChange}
         handleSubmit={saveReview}
+        loading={loading}
       />
       <DebugStates reviewId={reviewId} fieldValues={fieldValues} />
     </div>
