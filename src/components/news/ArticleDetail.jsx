ad8e1e34bf4fb7d7681434e14cdecc81b811ea1e
Suin Kim <soo.officer@gmail.com>
2022-01-12 16:24:28 +0900

    포스팅 삭제 구현

--- src/components/news/ArticleDetail.jsx
+++ src/components/news/ArticleDetail.jsx
@@ -1,11 +1,31 @@
-import { Link } from 'react-router-dom';
-import { useApiAxios } from 'api/base';
 import { useEffect } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import { useApiAxios } from 'api/base';
+import LoadingIndicator from 'components/LoadingIndicator';
 
 function ArticleDetail({ articleId }) {
+  const navigate = useNavigate();
+
   const [{ data: article, loading, error }, refetch] = useApiAxios(
     `/news/api/articles/${articleId}/`,
   );
+
+  const [{ loading: deleteLoading, error: deleteError }, deleteArticle] =
+    useApiAxios(
+      {
+        url: `/news/api/articles/${articleId}/`,
+        method: 'DELETE',
+      },
+      { manual: true },
+    );
+
+  const handleDelete = () => {
+    if (window.confirm('Are you sure?')) {
+      deleteArticle().then(() => {
+        navigate('/news/');
+      });
+    }
+  };
 
   useEffect(() => {
     refetch();
@@ -13,8 +33,12 @@ function ArticleDetail({ articleId }) {
 
   return (
     <div>
-      {loading && '로딩중...'}
-      {error && '로딩 중 에러가 발생했습니다.'}
+      {loading && <LoadingIndicator />}
+      {deleteLoading && <LoadingIndicator>삭제 중 ...</LoadingIndicator>}
+      {error &&
+        `로딩 중 에러가 발생했습니다. (${error.response.status} ${error.response.statusText})`}
+      {deleteError &&
+        `삭제 요청 중 에러가 발생했습니다. (${deleteError.response.status} ${deleteError.response.statusText})`}
       {article && (
         <>
           <h3 className="text-2xl my-5">{article.title}</h3>
@@ -35,6 +59,13 @@ function ArticleDetail({ articleId }) {
         <Link to={`/news/${articleId}/edit/`} className="hover:text-red-400">
           수정하기
         </Link>
+        <button
+          disabled={deleteLoading}
+          onClick={handleDelete}
+          className="hover:text-red-400"
+        >
+          삭제하기
+        </button>
       </div>
     </div>
   );
