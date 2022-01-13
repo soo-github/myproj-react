12a33b72417b0d18911d1e451a02da1e614ae80c
Suin Kim <soo.officer@gmail.com>
2022-01-13 11:43:59 +0900

    article form에서 photo=null이 전달되는 버그를 해결

--- src/components/news/ArticleForm.jsx
+++ src/components/news/ArticleForm.jsx
@@ -4,6 +4,7 @@ import H2 from 'components/H2';
 import LoadingIndicator from 'components/LoadingIndicator';
 import useFieldValues from 'hooks/useFieldValues';
 import { useApiAxios } from 'api/base';
+import { useEffect } from 'react';
 
 const INIT_FIELD_VALUES = { title: '', content: '' };
 
@@ -38,9 +39,19 @@ function ArticleForm({ articleId, handleDidSave }) {
     { manual: true },
   );
 
-  const { fieldValues, handleFieldChange } = useFieldValues(
+  const { fieldValues, handleFieldChange, setFieldValues } = useFieldValues(
     article || INIT_FIELD_VALUES,
   );
+
+  useEffect(() => {
+    // 서버로 photo=null이 전달이 되면, 아래 오류가 발생
+    //   - The submitted data was not a file. Check the encoding type on the form.
+    //   - 대응 : fieldValues에서 photo만 제거해주거나, photo=null이라면 빈 문자열로 변경
+    setFieldValues((prevFieldValues) => ({
+      ...prevFieldValues,
+      photo: '',
+    }));
+  }, [article]);
 
   const handleSubmit = (e) => {
     e.preventDefault();
