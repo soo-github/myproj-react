859a78ab60bddcf9c7ccafdc6c802188e1663e53
Suin Kim <soo.officer@gmail.com>
2022-01-17 17:22:45 +0900

    PageLogin. username/password를 통한 token 획득 구현

--- src/pages/accounts/PageLogin.jsx
+++ src/pages/accounts/PageLogin.jsx
@@ -7,15 +7,18 @@
 
 import DebugStates from 'components/DebugStates';
 import useFieldValues from 'hooks/useFieldValues';
-import { Navigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import { useApiAxios } from 'api/base';
+import Button from 'components/Button';
 
 const INIT_FIELD_VALUES = { username: '', password: '' };
 
 function PageLogin() {
+  const navigate = useNavigate();
+
   const [{ loading, error }, requestToken] = useApiAxios(
     {
-      url: '/accounts/api/token',
+      url: '/accounts/api/token/',
       method: 'POST',
     },
     { manual: true },
@@ -28,11 +31,13 @@ function PageLogin() {
 
     requestToken({ data: fieldValues }).then((response) => {
       const { access, refresh } = response.data;
-
+      // TODO: access/refresh token을 브라우저 어딘가에 저장해야 합니다.
+      // 저장해서 페이지 새로고침이 발생하더라도 그 token이 유실되지 않아야 합니다.
       console.log('access:', access);
       console.log('refresh:', refresh);
 
-      Navigate('/');
+      // 인증 후, 이동할 주소를 지정합니다.
+      navigate('/');
     });
   };
 
@@ -40,35 +45,37 @@ function PageLogin() {
     <div>
       <h2>Login</h2>
 
-      <div className="flex">
-        <span>name:</span>
-        <div className="ml-5 mr-5 border border-3 rounded">
+      {error?.response?.status === 401 && (
+        <div className="text-red-400">로그인에 실패했습니다.</div>
+      )}
+
+      <form onSubmit={handleSubmit}>
+        <div className="my-3">
           <input
             type="text"
             name="username"
             value={fieldValues.username}
             onChange={handleFieldChange}
+            placeholder="username"
+            className="p-3 bg-gray-100 focus:outline-none focus:border focus:border-gray-400 w-full"
           />
         </div>
 
-        <span>password:</span>
-        <div className="ml-5 border border-3 rounded">
+        <div className="my-3">
           <input
             type="password"
             name="password"
             value={fieldValues.password}
             onChange={handleFieldChange}
+            placeholder="password"
+            className="p-3 bg-gray-100 focus:outline-none focus:border focus:border-gray-400 w-full"
           />
         </div>
 
-        <br />
-        <div>
-          <button className="bg-red-300 hover:bg-red-400 ml-5 p-2 rounded">
-            로그인
-          </button>
-        </div>
-      </div>
-      <DebugStates fieldValues={fieldValues} />
+        <Button>로그인</Button>
+      </form>
+
+      <DebugStates fieldValues={fieldValues} error={error} />
     </div>
   );
 }
