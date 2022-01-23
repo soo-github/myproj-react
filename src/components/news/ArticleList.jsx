948b1e5b314d9c3a7c830b69a3ef8667a8384e14
Suin Kim <soo.officer@gmail.com>
2022-01-23 09:05:51 +0900

    news페이지 카드 레이아웃 적용

--- src/components/news/ArticleList.jsx
+++ src/components/news/ArticleList.jsx
@@ -34,18 +34,20 @@ function ArticleList() {
 
   return (
     <div className="my-5">
-      <h3>뉴스 기사 목록을 보여줄 것입니다.</h3>
       {loading && '로딩 중 ...'}
       {error && '로딩 중 에러가 발생했습니다.'}
-      {articleList &&
-        articleList.map((article, index) => (
-          <div
-            key={article.id}
-            className="w-full md:w-1/2 px-4 transition-transform hover:-translate-y-5 duration-300"
-          >
-            <ArticleSummary article={article} key={index} />
-          </div>
-        ))}
+      {articleList && (
+        <div className="flex flex-wrap">
+          {articleList.map((article, index) => (
+            <div
+              key={article.id}
+              className="w-full md:w-1/2 px-4 transition-transform hover:-translate-y-5 duration-300"
+            >
+              <ArticleSummary article={article} key={index} />
+            </div>
+          ))}
+        </div>
+      )}
       <DebugStates articleList={articleList} loading={loading} error={error} />
     </div>
   );
--- src/components/news/ArticleSummary.jsx
+++ src/components/news/ArticleSummary.jsx
@@ -2,16 +2,16 @@ import { Link } from 'react-router-dom';
 
 function ArticleSummary({ article }) {
   return (
-    <div>
+    <div className="bg-gray-100 border-gray-100 border-2 rounded-lg overflow-hidden mb-10">
       {article.photo && (
-        <img
-          src={article.photo}
-          alt={article.title}
-          className="w-5 h-5 mr-1 inline"
-        />
+        <img src={article.photo} alt={article.title} className="w-full" />
       )}
-      <Link to={`/news/${article.id}/`}>{article.title}</Link>
-      <p>by {article.author.username}</p>
+      <div className="p-8 sm:p-9 md:p-7 xl:p-9">
+        <h3>
+          <Link to={`/news/${article.id}/`}>{article.title}</Link>
+          <p>by {article.author.username}</p>
+        </h3>
+      </div>
     </div>
   );
 }
